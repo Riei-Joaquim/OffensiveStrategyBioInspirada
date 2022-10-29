db1adc99038de7800d69fd72682e3f5d039e5f30
Lucas Dias Maciel <ldm@cin.ufpe.br>
2022-10-28 23:47:13 -0300

    wip

--- AttackInformationTree.py
+++ AttackInformationTree.py
@@ -16,37 +16,44 @@ class AttackInformationTree:
         self.dribbling = {}
 
     def insert(self, inputData: InputData):
-        inputDataDiscrete = inputData.discrete_function()
-        if inputDataDiscrete.action == 0:
-            if inputDataDiscrete.ashoot not in self.shooting:
-                self.shooting[inputDataDiscrete.ashoot] = {}
-            if inputDataDiscrete.dshoot not in self.shooting[inputDataDiscrete.ashoot]:
-                self.shooting[inputDataDiscrete.ashoot][inputDataDiscrete.dshoot] = {
-                    "fail": 0, "succ": 0, "sucRate": 0.0, "preference": 0.0}
-            if inputDataDiscrete.success:
-                self.shooting[inputDataDiscrete.ashoot][inputDataDiscrete.dshoot]["succ"] += 1
+        inputDataCopy = inputData
+        inputDataCopy.discrete_function()
+        if inputDataCopy.action == 0:
+            if inputDataCopy.ashoot not in self.shooting:
+                self.shooting[inputDataCopy.ashoot] = {}
+            if inputDataCopy.dshoot not in self.shooting[inputDataCopy.ashoot]:
+                self.shooting[inputDataCopy.ashoot][inputDataCopy.dshoot] = {
+                    "fail": 0, "succ": 0, "sucRate": 0, "preference": 0}
+            if inputDataCopy.success:
+                self.shooting[inputDataCopy.ashoot][inputDataCopy.dshoot][
+                    "succ"] += 1
             else:
-                self.shooting[inputDataDiscrete.ashoot][inputDataDiscrete.dshoot]["fail"] += 1
-        elif inputDataDiscrete.action == 1:
-            if inputDataDiscrete.apass not in self.passing:
-                self.passing[inputDataDiscrete.apass] = {}
-            if inputDataDiscrete.dintp not in self.passing[inputDataDiscrete.apass]:
-                self.passing[inputDataDiscrete.apass][inputDataDiscrete.dintp] = {
-                    "fail": 0, "succ": 0, "sucRate": 0.0, "preference": 0.0}
-            if inputDataDiscrete.success:
-                self.passing[inputDataDiscrete.apass][inputDataDiscrete.dintp]["succ"] += 1
+                self.shooting[inputDataCopy.ashoot][inputDataCopy.dshoot][
+                    "fail"] += 1
+        elif inputDataCopy.action == 1:
+            if inputDataCopy.apass not in self.passing:
+                self.passing[inputDataCopy.apass] = {}
+            if inputDataCopy.dintp not in self.passing[inputDataCopy.apass]:
+                self.passing[inputDataCopy.apass][inputDataCopy.dintp] = {
+                    "fail": 0, "succ": 0, "sucRate": 0, "preference": 0}
+            if inputDataCopy.success:
+                self.passing[inputDataCopy.apass][inputDataCopy.dintp][
+                    "succ"] += 1
             else:
-                self.passing[inputDataDiscrete.apass][inputDataDiscrete.dintp]["fail"] += 1
-        elif inputDataDiscrete.action == 2:
-            if inputDataDiscrete.ddrib not in self.dribbling:
-                self.dribbling[inputDataDiscrete.ddrib] = {}
-            if inputDataDiscrete.dintd not in self.dribbling[inputDataDiscrete.ddrib]:
-                self.dribbling[inputDataDiscrete.ddrib][inputDataDiscrete.dintd] = {
-                    "fail": 0, "succ": 0, "sucRate": 0.0, "preference": 0.0}
-            if inputDataDiscrete.success:
-                self.dribbling[inputDataDiscrete.ddrib][inputDataDiscrete.dintd]["succ"] += 1
+                self.passing[inputDataCopy.apass][inputDataCopy.dintp][
+                    "fail"] += 1
+        elif inputDataCopy.action == 2:
+            if inputDataCopy.ddrib not in self.dribbling:
+                self.dribbling[inputDataCopy.ddrib] = {}
+            if inputDataCopy.dintd not in self.dribbling[inputDataCopy.ddrib]:
+                self.dribbling[inputDataCopy.ddrib][inputDataCopy.dintd] = {
+                    "fail": 0, "succ": 0, "sucRate": 0, "preference": 0}
+            if inputDataCopy.success:
+                self.dribbling[inputDataCopy.ddrib][inputDataCopy.dintd][
+                    "succ"] += 1
             else:
-                self.dribbling[inputDataDiscrete.ddrib][inputDataDiscrete.dintd]["fail"] += 1
+                self.dribbling[inputDataCopy.ddrib][inputDataCopy.dintd][
+                    "fail"] += 1
 
     def __str__(self):
         return str("Shooting: " + json.dumps(self.shooting, sort_keys=True, indent=4)) + \
